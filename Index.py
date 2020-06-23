edea3f6289d0579b1a7112e8033812f7f7a05e39
Pooja <upooja2290@gmail.com>
2020-06-23 16:37:21 -0500

    first commit

---
+++ Index.py
@@ -0,0 +1,19 @@
+import flask
+from flask import Flask
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    return flask.render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    return flask.render_template('about.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
